893a6a23fbe6493c8d2de9d7470e62d0af88597f
rifasorteobri-git <rifasorteobri@gmail.com>
2025-12-04 23:33:07 -0500

    27 cambio

--- routes/boletosRoutes.js
+++ routes/boletosRoutes.js
@@ -34,7 +34,7 @@ router.post('/administrador/boletos/asignar/:rifaId', async (req, res) => {
 //Buscar por nombre
 router.get('/administrador/boletos/buscar/:nombre', async (req, res) => {
     try {
-        const nombre = req.query.nombre;
+        const nombre = req.params;
         const { data, error } = await supabase
         .from('boletos')
         .select('*')
