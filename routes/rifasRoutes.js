51060541adf165848d0baa8ab870bd4cd8e99777
rifasorteobri-git <rifasorteobri@gmail.com>
2025-12-09 00:45:31 -0500

    44 cambio

--- routes/rifasRoutes.js
+++ routes/rifasRoutes.js
@@ -110,6 +110,18 @@ router.get('/administrador/obtenerRifa/:id', async (req, res) => {
   }
 })
 
+//Obtener el total de vendidos
+router.get('/administrador/obtenerVendidosRifa/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { data, error } = await supabase.from('boletos').select('*', {count: 'exact', head: true}).eq('rifa_id', id).eq('estado', 'vendido');
+    if (error) throw error;
+    res.json(data);
+  } catch(err) {
+    res.status(500).json({ error: err.message || err });
+  }
+})
+
 // Editar rifa
 router.put('/administrador/editarRifa/:id', upload.single('imagenRifas'), async (req, res) => {
   try {
