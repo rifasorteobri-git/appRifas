2d655421a68b759c80679b623e21f21b166b96f1
rifasorteobri-git <rifasorteobri@gmail.com>
2025-12-10 18:10:38 -0500

    52 cambio

--- utils/generarBoletos.js
+++ utils/generarBoletos.js
@@ -1,17 +1,22 @@
 //función para generar boletos
 function generarBoletos(cantidad) {
-  if (!Number.isInteger(cantidad) || cantidad < 1 || cantidad > 1000) {
-    throw new Error('cantidad debe ser entero entre 1 y 1000');
+  if (!Number.isInteger(cantidad) || cantidad < 1 || cantidad > 900) {
+    throw new Error('cantidad debe ser entero entre 1 y 900');
   }
-  const todos = [];
-  for (let i = 0; i <= 999; i++) todos.push(String(i).padStart(3, '0'));
 
-  // Fisher-Yates
+  const todos = [];
+  for (let i = 100; i <= 999; i++) {
+    todos.push(String(i)); // "100" a "999"
+  }
+
+  // Fisher-Yates shuffle
   for (let i = todos.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
     [todos[i], todos[j]] = [todos[j], todos[i]];
   }
+
   return todos.slice(0, cantidad);
 }
+
 
 module.exports = generarBoletos;
